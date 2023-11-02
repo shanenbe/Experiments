ca67f802346e720e764818c7310a8ca483352b8b
shanenbe <stefan.hanenberg@uni-due.de>
2023-11-02 12:06:00 +0100

    ...

--- 2023_Indentation_JSON/code.js
+++ 2023_Indentation_JSON/code.js
@@ -218,7 +218,10 @@ document.experiment_definition(
             "correct result will be shown to you."],
         pre_run_instruction:"Please put your fingers on the number buttons.\n\nWhen you press [Enter] the first task will be shown.",
         finish_pages:["Thanks for participating. When you press [Enter], the experiment's data will be downloaded.\n\n" +
-        "If you want to contribute to research, you can send the downloaded file to stefan.hanenberg@uni-due.de."],
+        "If you want to contribute to research, you can send the downloaded file to stefan.hanenberg@uni-due.de. \n\n" +
+        "Additionally to the experiment data, it is necessary that you send the signed \n" +
+        "information sheet to the same mail address. You can download the information sheet at:\n\n" +
+        "https://github.com/shanenbe/Experiments/blob/main/2023_Indentation_JSON/Aufkl%C3%A4rungbogen%20und%20Einverst%C3%A4ndniserkl%C3%A4rung%20zur%20Teilnahme%20an%20der%20Studie.pdf"],
         layout:[
             {variable:"Indentation",treatments:["indented", "non-indented"]},
             {variable:"Number_of_fields",treatments:["1", "3", "5"]},
