a63a4c47a986092d78225bbac68eeae9c1387df3
shanenberg <stefan.hanenberg@uni-due.de>
2024-11-17 15:07:49 +0100

    TypeDSL experiment

--- 2024_LanguageTypesDSL_Readability/typescript/experiment_configuration.ts
+++ 2024_LanguageTypesDSL_Readability/typescript/experiment_configuration.ts
@@ -50,7 +50,7 @@ let experiment_configuration_function = (writer: Experiment_Output_Writer) => { 
             "Let's see on the next page how such type systems can be represented using ordinary Java code."
 
         );// @ts-ignore
-            window.MathJax.typeset();},
+            document.update_mathjax();},
 
         ()=>{writer.print_string_on_stage("We assume in the given experiment, that (in a language implementation) each language construct is defined in it's own class (extending some root class LTerm) and that this class has a method <code>Type type_of(Environment e) {...}</code> " +
             "that returns the type of the given language construct. For the type <code>BOOL</code> and <code>NUMBER</code> we assume the existence of corresponding constants.<br><br>" +
@@ -93,7 +93,7 @@ let experiment_configuration_function = (writer: Experiment_Output_Writer) => { 
             "Before starting the experiment, let's see some examples on the next pages."
 
         );// @ts-ignore
-            window.MathJax.typeset();},
+            document.update_mathjax();},
 
         ()=>{writer.print_string_on_stage("The experiment asks, in what position in a given expression (with given type rules) a type error appears - and possibly there is no type error. " +
             "In the experiment, there are two different literals (that have the type <code>BOOL</code> or <code>NUMBER</code>) and one additional literal that has a function type (either <code>BOOL->NUMBER</code> or <code>NUMBER->BOOL</code>)." +
