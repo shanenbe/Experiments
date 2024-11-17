ddf024632b8a50e35b3123247669297a707feeb2
shanenberg <stefan.hanenberg@uni-due.de>
2024-11-17 21:32:26 +0100

    TypeDSL experiment

---
+++ 20xx_ToAmen/experiment.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/*!************************************************!*\
+  !*** ./typescript/experiment_configuration.ts ***!
+  \************************************************/
+document.getElementById("STAGE").innerHTML = "<p>" +
+    "$\\scriptsize{\\mathrm{somethingelse:NUMBER}\\rightarrow\\mathrm{BOOL}}$";
+// @ts-ignore
+MathJax.typeset();
+
+/******/ })()
+;
+//# sourceMappingURL=experiment.js.map
