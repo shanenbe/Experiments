82eeadaff987b4ce08a05f7cae6efc14dd64a0ce
shanenberg <stefan.hanenberg@uni-due.de>
2025-03-13 14:38:56 +0100

    Init Bool expression experiment

---
+++ 2024_BooleanExpressions_01/trial04/typescript/Bool_Expression_Generator.ts
@@ -0,0 +1,83 @@
+import {
+    do_random_array_sort,
+    random_integer_up_to_excluding,
+    random_lower_case_letter_except
+} from "../../../N-of-1-Experimentation/modules/Experimentation/Experimentation.js";
+import {repeat_, repeat_n_times} from "../../../N-of-1-Experimentation/modules/utils/loops/loop.js";
+
+let OR = "||";
+let AND = "&&";
+let TRUE = "true";
+let FALSE = "false";
+
+
+function set_and_snippet_true(andSnippet: string[]) {
+    for(let c = 0; c < andSnippet.length; c++) {
+        andSnippet[c]=TRUE;
+    }
+}
+
+function set_and_snippet_false(andSnippet: string[]) {
+    andSnippet[0] = FALSE;
+    for(let c = 1; c < andSnippet.length; c++) {
+        andSnippet[c] = random_integer_up_to_excluding(2)==1?TRUE:FALSE;
+    }
+    let new_array = do_random_array_sort(andSnippet);
+    andSnippet.splice(0, andSnippet.length);
+
+    for (let n of new_array) {
+        andSnippet.push(n);
+    }
+}
+
+function set_answer_position_into_and_snippets(and_snippets: string[][], answer_position: number) {
+    for (let i = 0; i < and_snippets.length; i++) {
+        if(i== (answer_position - 1)) {
+            set_and_snippet_true(and_snippets[i]);
+        } else {
+            set_and_snippet_false(and_snippets[i]);
+        }
+    }
+}
+
+function print_and_snippets_into_string(and_snippets: string[][], wants_new_line: boolean) {
+    let ret = [];
+    for(let and_snippet of and_snippets) {
+        ret.push(and_snippet.join(" && "));
+    }
+    return wants_new_line? ret.join("\n|| ") : ret.join(" || ");
+}
+
+export function generate_boolean_expression_as_string(
+                                                        treat_var_format:string,
+                                                        terms_per_and: number,
+                                                        answerPos: number
+                                                     ):string {
+    let number_of_lines = 5;
+    let number_of_literals = terms_per_and * number_of_lines;
+    let literals:string[] = [];
+
+    // let operators: string[] = [OR, OR, OR, AND, AND, AND, AND];
+
+    // for(let c=operators.length; c < number_of_literals-1; c++) {
+    //     operators.push(random_integer_up_to_excluding(2)==1?AND:OR);
+    // }
+
+    // operators = do_random_array_sort(operators);
+    let and_snippets = [
+                       ];
+    repeat_(number_of_lines)._times( () => {
+        and_snippets.push([]);
+        repeat_(terms_per_and)._times( () =>{
+            and_snippets[and_snippets.length-1].push(FALSE);
+        });
+    });
+
+    set_answer_position_into_and_snippets(and_snippets, answerPos);
+    let ret =  print_and_snippets_into_string(and_snippets, treat_var_format=="MultiLine");
+
+    return ret;
+}
+
+// let dummy = generate_boolean_expression_as_string("MultiLine", 3);
+// console.log(dummy);
---
+++ 2024_BooleanExpressions_01/trial04/typescript/experiment_configuration.ts
@@ -0,0 +1,149 @@
+import {BROWSER_EXPERIMENT} from "../../../N-of-1-Experimentation/modules/Experimentation/Browser_Output_Writer.js";
+import {
+    alternatives,
+    Experiment_Output_Writer, keys, random_array_element, Reaction_Time,
+    SET_SEED
+} from "../../../N-of-1-Experimentation/modules/Experimentation/Experimentation.js";
+import {Task} from "../../../N-of-1-Experimentation/modules/Experimentation/Task.js";
+import {
+} from "./Bool_Expression_Generator.js";
+import {generate_boolean_expression_as_string} from "./Bool_Expression_Generator.js";
+
+
+let SEED = "42";
+
+SET_SEED(SEED);
+
+let experiment_configuration_function = (writer: Experiment_Output_Writer) => { return {
+
+    experiment_name: "SQL-WithVersusSubquery",
+    seed: SEED,
+
+    introduction_pages:                 [
+                                            ()=>writer.print_string_on_stage("This is your hello world experiment.")
+                                        ],
+
+    pre_run_training_instructions:      writer.string_page_command(
+                                            "You entered the training phase. You can skip the training by pressing [Esc]."
+                                        ),
+
+    pre_run_experiment_instructions:    writer.string_page_command(
+                                            "You entered the experiment phase."
+                                        ),
+
+    post_questionnaire           :      [
+                                            alternatives(
+                                                            "Age",
+                                                            "What's your age??",
+                                                            [
+                                                                "younger than 18",                  "between 18 and (excluding) 25",        "between 25 and (excluding) 30",
+                                                                "between 30 and (excluding) 35",    "between 35 and (excluding) 40",        "40 or older"
+                                                            ]
+                                            ),
+
+                                            alternatives(
+                                                            "Status",
+                                                            "What is your current working status?",
+                                                             [
+                                                                 "Undergraduate student (BSc not yet finished)",        "Graduate student (at least BSc finished)",         "PhD student",
+                                                                 "Professional software developer",                     "Teacher",                                          "Other"
+                                                             ]
+                                            ),
+
+                                            alternatives(
+                                                            "Studies",
+                                                            "In case you study, what's your subject?",
+                                                            [
+                                                                "I do not study",                       "Computer science",     "computer science related (such as information systems, aka WiInf)",
+                                                                "something else in natural sciences",   "something else"
+                                                            ]
+                                            ),
+
+                                            alternatives(
+                                                            "YearsOfExperience",
+                                                            "How many years of experience do you have in software industry?",
+                                                            [
+                                                                "none",                                                 "less than or equal 1 year",    "more than 1 year, but less than or equal 3 years",
+                                                                "more than 3 years, but less than or equal 5 year",     "more than 5 years"
+                                                            ]
+                                            ),
+
+                                            alternatives(
+                                                            "impression",
+                                                            "What statement describes best your impression \n\ of the experiment?",
+                                                            [
+                                                                    "I do not think that there was a difference between the notations",
+                                                                    "The inference notation made it slightly easier for me",
+                                                                    "Java made it slightly easier for me",
+                                                                    "The inference notation made it much easier for me",
+                                                                    "Java made it much easier for me",
+                                                            ]
+                                            )
+    ],
+
+    training_configuration: {
+        can_be_cancelled: true,
+        can_be_repeated: true
+    },
+
+    finish_pages: [
+                        writer.string_page_command(
+                            "<p>Almost done. Next, the experiment data will be downloaded (after pressing [Enter]).<br><br>" +
+                            "Please, send the " +
+                            "downloaded file to the experimenter: " + "<a href='mailto:stefan.hanenberg@uni-due.de'>stefan.hanenberg@uni-due.de</a></p>" +
+                            "<p>By sending that mail, you agree that " +
+                            "your (anonymized) data will be used for scientific analyses where your data (together with others in an " +
+                            "anonymized way) will be published.<br><br>I.e., you agree with the information sheet, see " +
+                            "<a href='https://github.com/shanenbe/Experiments/blob/main/2024_TypeSystems_ConstructorCall_Flat/Agreement.pdf' target='_blank'>here</a>. " +
+                            "Note, that it it no longer necessary to send a signed version of the agreement to the experimenter.<br><br>" +
+                            "After sending your email, you can close this window.</p>" +
+                            "<p>Many thanks for your participation.<br>" +
+                            "-Stefan Hanenberg</p>"
+                        )
+                    ],
+
+    layout: [
+        { variable: "Format",  treatments: ["OneLine", "MultiLine"]},
+        { variable: "TermsPerAnd",  treatments: ["1", "2", "3"]},
+        { variable: "AnswerPosition",  treatments: ["1", "2", "3", "4", "5"]}
+    ],
+
+    repetitions: 4,
+
+    measurement: Reaction_Time(keys(["0", "1"])),
+
+    task_configuration:    (t:Task) => {
+
+        let string_to_show;
+
+        string_to_show = generate_boolean_expression_as_string(
+                                                                  t.treatment_value("Format"),
+                                                                  parseInt(t.treatment_value("TermsPerAnd")),
+                                                                  parseInt(t.treatment_value("AnswerPosition"))
+                                                              );
+
+
+
+        t.expected_answer = t.treatment_value("AnswerPosition");
+
+        t.do_print_task = () => {
+            writer.clear_stage();
+            writer.print_html_on_stage(
+                "<div class='sourcecode'>"
+                + writer.convert_string_to_html_string(string_to_show)
+                + "</div>"
+            );
+        };
+
+        t.accepts_answer = (s) => true;
+
+        t.do_print_after_task_information = () => {
+            writer.print_error_string_on_stage(writer.convert_string_to_html_string(
+                "The correct answer was: " + t.expected_answer + "\n\n" +
+                "In case, you feel not concentrated enough, make a short break.\n\n" +
+                "Press [Enter] to go on. "));
+        }
+    }
+}};
+
+BROWSER_EXPERIMENT(experiment_configuration_function);
