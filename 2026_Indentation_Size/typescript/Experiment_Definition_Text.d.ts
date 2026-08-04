f0bb7b6ac926d27a06a0566e7bb3fb8225672b96
shanenberg <stefan.hanenberg@uni-due.de>
2026-08-04 17:31:11 +0200

    ...

--- 2026_Indentation_Size/typescript/Experiment_Definition_Text.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function intro_pages(): string[];
-export declare function pre_run_training_instructions(): string;
-export declare function pre_run_experiment_instructions(): string;
-export declare function finish_pages(): string;
